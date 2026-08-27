50befa7681c359a4f12beb4f4f73cebfd76969c7
- Rahulwa <148678357+sudormrfrahul@users.noreply.github.com>
2026-08-27 23:50:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// const song = document.getElementById("music");
+let best = document.getElementById("best");
+function msg() {
+  best.innerText = "Rahulwa: East or West, My akkoi is the Best. 💕";
+}
